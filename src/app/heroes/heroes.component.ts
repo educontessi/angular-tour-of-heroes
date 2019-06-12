5ecf4732d161404a509f668e0abfda83e1cfb690
Eduardo Contessi <Eduardo.Contessi@thomsonreuters.com>
2019-06-12 09:52:29 -0300

    Capítutlo 4 - Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -10,8 +11,8 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
   selectedHero: Hero;
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
@@ -24,6 +25,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
